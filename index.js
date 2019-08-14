838ccf3098d71f16152a488adaa50fb60b5a6180
Tina Huang <tina@transposit.com>
2019-08-13 17:35:16 -0700

    put in some front matter stuff

--- index.js
+++ index.js
@@ -1,48 +1,9 @@
-const fs = require('fs');
-const process = require('process');
-const {spawnSync} = require('child_process');
-
-const {TOKEN, USERNAME} = process.env;
-
-function runCommand(commandString, options) {
-  const [command, ...args] = commandString.match(/(".*?")|(\S+)/g);
-  const cmd = spawnSync(command, args, options);
-
-  const errorString = cmd.stderr.toString();
-  if (errorString) {
-    console.log(errorString)
-  }
-  if (cmd.status !== 0) {
-    throw new Error("Unsuccessful cmd: " + commandString + "\n" + errorString);
-  }
-  return errorString;
-}
-
-function sync(service) {
-  var name = service.name;
-  var maintainer = service.maintainer;
-
-  // change the cwd to /tmp
-  process.chdir('/tmp');
-
-  // remove the dir if it's already in tmp
-  const dest = `/tmp/${name}`;
-  runCommand(`rm -rf ${dest}`);
-
-  // clone the repository and set it as the cwd
-  const gitRepositoryURL = `https://${USERNAME}:${TOKEN}@console.transposit.com/git/${maintainer}/${name}`;
-  runCommand(`git clone ${gitRepositoryURL}`);
-  process.chdir(dest);
-
-  return JSON.parse(fs.readFileSync('manifest.json').toString());
-}
+const fm = require('front-matter');
+const CryptoJS = require("crypto-js");
 
 exports.handler = async function (event, context, callback) {
-  // install git binary
-  await require('lambda-git')();
+  var parsedWordArray = CryptoJS.enc.Base64.parse(event.content.replace(/\n/g,""));
+  var parsedStr = parsedWordArray.toString(CryptoJS.enc.Utf8);
 
-  let manifest = sync(event.service);
-  callback(null, {
-    manifest: manifest
-  });
+  callback(null, fm(parsedStr));
 }
