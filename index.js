aef274cc82b8e3f4495b7094f67fcf1a92e0d900
Tina Huang <tina@transposit.com>
2019-08-13 18:55:25 -0700

    use proxy event

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const fm = require('front-matter');
 const CryptoJS = require("crypto-js");
 
 exports.handler = async function (event, context, callback) {
-  var parsedWordArray = CryptoJS.enc.Base64.parse(event.content.replace(/\n/g,""));
+  var parsedWordArray = CryptoJS.enc.Base64.parse(event.body.replace(/\n/g,""));
   var parsedStr = parsedWordArray.toString(CryptoJS.enc.Utf8);
 
   callback(null, fm(parsedStr));
