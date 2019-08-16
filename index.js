20204167e3332724dbc106545f93133ffdd59b97
Tina Huang <tina@transposit.com>
2019-08-15 19:13:39 -0700

    default to base64

--- index.js
+++ index.js
@@ -4,11 +4,11 @@ const CryptoJS = require("crypto-js");
 exports.handler = async function (event, context, callback) {
   var content = JSON.parse(event.body).content;
   var parsedStr;
-  if (event.queryStringParameters && event.queryStringParameters.base64 === 'true') {
+  if (event.queryStringParameters && event.queryStringParameters.base64 === 'false') {
+    parsedStr = content;
+  } else {
     var parsedWordArray = CryptoJS.enc.Base64.parse(content.replace(/\n/g,""));
     parsedStr = parsedWordArray.toString(CryptoJS.enc.Utf8);
-  } else {
-    parsedStr = content;
   }
 
   callback(null, {
